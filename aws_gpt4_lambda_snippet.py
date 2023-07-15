5ae86ea88652e8faf29d4e01b646594196889327
Ethycs <Ethycs@users.noreply.github.com>
2023-07-14 18:15:01 -0700

    Basic commit

---
+++ aws_gpt4_lambda_snippet.py
@@ -0,0 +1,31 @@
+import boto3
+from botocore.exceptions import BotoCoreError, ClientError
+
+def get_secret(secret_name, region_name):
+    session = boto3.session.Session()
+    client = session.client(
+        service_name='secretsmanager',
+        region_name=region_name
+    )
+
+    try:
+        get_secret_value_response = client.get_secret_value(
+            SecretId=secret_name
+        )
+    except ClientError as e:
+        raise Exception("Couldn't retrieve the secret") from e
+    else:
+        if 'SecretString' in get_secret_value_response:
+            return get_secret_value_response['SecretString']
+        else:
+            return base64.b64decode(get_secret_value_response['SecretBinary'])
+
+def lambda_handler(event, context):
+    secret = get_secret('MyAPIKey', 'us-west-2')
+    print(f"API Key: {secret}")
+    # Now use the secret (API Key) to make your API request
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
