b614e48b0b7b1ce4072f96bad126793ba0b4f4b4
Ethycs <Ethycs@users.noreply.github.com>
2023-08-01 11:56:07 -0700

    Fix gitignore and update low latency secrets

--- test-sam/secrets_api/app.py
+++ test-sam/secrets_api/app.py
@@ -1,27 +1,37 @@
 import os
+
 import json
-# import requests
+import requests
 from aws_secretsmanager_caching import SecretCache, SecretCacheConfig
 
 def lambda_handler(event, context):
-    # Instantiate secret cache with specific configuration
-    secret_cache = SecretCache(SecretCacheConfig())
+    # # Instantiate secret cache with specific configuration
+    # secret_cache = SecretCache(SecretCacheConfig())
 
-    # Get the secrets from the cache
-    secret_object = secret_cache.get_secret_string('test_secret')
+    # # Get the secrets from the cache
+    # secret_object = secret_cache.get_secret_string('test_secret')
 
-    # Secrets are returned as a JSON string, so load it as a Python object
-    secrets = json.loads(secret_object)
+    # # Secrets are returned as a JSON string, so load it as a Python object
+    # secrets = json.loads(secret_object)
 
     # # Use the secrets to authenticate an HTTP request
     # response = requests.get(
     #     'http://example.com',
     #     auth=(secrets['username'], secrets['password'])
     # )
+    headers = {"X-Aws-Parameters-Secrets-Token": os.environ.get('AWS_SESSION_TOKEN')}
+    secrets_extension_endpoint = "http://localhost:" + \
+    "2773" + \
+    "/secretsmanager/get?secretId=" + \
+    "test_secret" 
+  
+    r = requests.get(secrets_extension_endpoint, headers=headers)
+  
+    secret = json.loads(r.text)["SecretString"] # load the Secrets Manager response into a Python dictionary, access the secret
 
     return {
         'statusCode': 200,
-        'body': secret_object,
+        'body': secret
     }
 
     return {
